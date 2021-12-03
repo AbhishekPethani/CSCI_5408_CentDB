bf73eaffc89915b6879bf064d19133bd36c39f6b
Abhishek Pareshbhai Pethani <ab823206@dal.ca>
2021-12-03 18:10:05 -0400

    Updated generated ERD code

--- src/main/java/data_modelling/GenerateERD.java
+++ src/main/java/data_modelling/GenerateERD.java
@@ -1,16 +1,16 @@
 package data_modelling;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class GenerateERD {
+public class GenerateERD implements IGenerateERD {
 	private String databaseName;
 	
 	public GenerateERD(String databaseName) {
@@ -22,51 +22,56 @@ public class GenerateERD {
 			throw new Exception("Database " + databaseName + " doesn't exist...");
 		}
 		File tables[] = getTables(databaseName);  
-		
 		File erdFile = new File("Databases/" + databaseName + "/" + databaseName + "_ERD.txt");
 		BufferedWriter bf = new BufferedWriter(new FileWriter(erdFile));
 		for(File table : tables) {
-			// System.out.println(parseTable(table));
-			bf.write(parseTable(table));
+			if(!(table.getName().contains("_ERD.txt") || table.getName().contains("_dump.sql"))) {
+				bf.write(parseTable(table));
+			}		
 		}
 		bf.close();
 	}
 	
-	public static String parseTable(File table) {
+	private static String parseTable(File table) {
 		String text = "";
 		String tableName = table.getName();
+		tableName = tableName.substring(0, tableName.lastIndexOf('.'));
 		text += tableName + "\n";
 		try {
-			Scanner sc = new Scanner(table);
-			String columnText = sc.nextLine();
+			BufferedReader br = new BufferedReader(new FileReader(table));
+			String columnText = br.readLine();
 			
-			List<String> columns = new ArrayList<String>();
-			Pattern p = Pattern.compile("<(.*?)>", Pattern.DOTALL);
-			Matcher m = p.matcher(columnText);
-						
-			while(m.find()) {
-				columns.add(m.group(1));
-			}
+			//List<String> columns = new ArrayList<String>();
+//			Pattern pattern = Pattern.compile("", Pattern.DOTALL);
+//			Matcher matcher = pattern.matcher(columnText);
+//						
+//			while(matcher.find()) {
+//				columns.add(matcher.group(1));
+//			}
+			List<String> columns = Arrays.asList(columnText.split("/"));
 			
 			for(String column : columns) {
 				text += "\t" + column + "\n";
 			}
 			
-			sc.close();
+			br.close();
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		return text;
 	}
 	
-	public static File[] getTables(String databaseName) {
+	private static File[] getTables(String databaseName) {
 		File database = new File("Databases/" + databaseName);
 		File tables[] = database.listFiles();
 		return tables;
 	}
 	
-	public static boolean databaseExist(String databaseName) {
+	private static boolean databaseExist(String databaseName) {
 		boolean databaseExist = false;
 		File database = new File("Databases/" + databaseName);
 		if(database.exists() && database.isDirectory()) {
---
+++ src/main/java/data_modelling/IGenerateERD.java
@@ -0,0 +1,5 @@
+package data_modelling;
+
+public interface IGenerateERD {
+	public void generateERD() throws Exception;
+}
