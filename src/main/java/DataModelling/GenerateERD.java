abb343d6cef83a08b01dc883a5850529de1234b1
Abhishek Pareshbhai Pethani <ab823206@dal.ca>
2021-12-06 17:11:01 -0400

    Added cardinalities 1:1 and 1:M

--- src/main/java/DataModelling/GenerateERD.java
+++ src/main/java/DataModelling/GenerateERD.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class GenerateERD implements IGenerateERD {
 	private String databaseName;
 	private static Map<String, String> primaryKeys;
-	private static Map<String, List<String>> foreignKeys;
+	private static Map<String, List<Map<String, String>>> foreignKeys;
 	
 	public GenerateERD(String databaseName) {
 		this.databaseName = databaseName;
@@ -23,7 +23,7 @@ public class GenerateERD implements IGenerateERD {
 		File erdFile = new File("Databases/" + databaseName + "/" + databaseName + "_ERD.txt");
 		BufferedWriter bf = new BufferedWriter(new FileWriter(erdFile));
 		primaryKeys = new HashMap<String, String>();
-		foreignKeys = new HashMap<String, List<String>>();
+		foreignKeys = new HashMap<String, List<Map<String, String>>>();
 		
 		// loop through all the tables and parse it
 		for(File table : tables) {
@@ -31,13 +31,14 @@ public class GenerateERD implements IGenerateERD {
 				bf.write(parseTable(table));
 			}		
 		}
+		
 		// call relationship methods
 		generateRelationships(primaryKeys, foreignKeys, bf);
 		bf.close();
 	}
 	
 	// method to find relationship between tables with cardinalities
-	private void generateRelationships(Map<String, String> primaryKeys, Map<String, List<String>> foreignKeys, BufferedWriter bf) throws IOException {
+	private void generateRelationships(Map<String, String> primaryKeys, Map<String, List<Map<String,String>>> foreignKeys, BufferedWriter bf) throws IOException {
 		// iterate through primaryKey map
 		for(Map.Entry<String, String> entry : primaryKeys.entrySet()) {
 			// get primary key from map
@@ -45,10 +46,20 @@ public class GenerateERD implements IGenerateERD {
 			// check if primary key is available in foreign key map
 			if(foreignKeys.containsKey(primaryKey)) {
 				// if primary key is available then get all the associated tables
-				List<String> foreignKeyTables = foreignKeys.get(primaryKey);
+				List<Map<String, String>> foreignKeyTables = foreignKeys.get(primaryKey);
+				
 				// print the relationship between primary key table with foreign key table
-				for(String foreignKeyTable : foreignKeyTables) {
-					bf.write(entry.getValue() + " - " + primaryKey + " (1) -> " + foreignKeyTable + " - " + primaryKey + " (M) \n");
+				for(Map<String, String> foreignKeyTable : foreignKeyTables) {
+					for(Map.Entry<String, String> e : foreignKeyTable.entrySet()) {
+						String constrain = e.getValue();
+						String cardinality = "";
+						if(constrain.equals("unique")){
+							cardinality = "1";
+						}else {
+							cardinality = "M";
+						}
+						bf.write(entry.getValue() + " - " + primaryKey + " (1) -> " + e.getKey() + " - " + primaryKey + " (" + cardinality +") \n");
+					}
 				}
 			}
 		}
@@ -79,24 +90,15 @@ public class GenerateERD implements IGenerateERD {
 				text += "\t" + c[0] + " " + c[1]; 
 				
 				// check if this column is primary key 
-				if(Arrays.asList(c).contains("pk")) {
-					// if primary key then add PK keyword into text
-					text += " PK";
-					// add table name and primary key into primaryKey map
-					primaryKeys.put(c[0], tableName);
+				if(isPrimaryKey(c, tableName)) {
+					// if primary key then add PRIMARY_KEY keyword into text
+					text += " PRIMARY_KEY";
 				}
 				
 				// check if this column is foreign key
-				if(Arrays.asList(c).contains("fk")) {
-					// if foreign key then add FK keyword into text
-					text += " FK";
-					// if the current column is not available into foreignKey map
-					// then add key and create empty list as a value 
-					if(!foreignKeys.containsKey(c[0])) {
-						foreignKeys.put(c[0], new ArrayList<String>());
-					}
-					// add table name into foreignKey map 
-					foreignKeys.get(c[0]).add(tableName);
+				if(isForeignKey(c, tableName)) {
+					// if foreign key then add FOREIGN_KEY keyword into text
+					text += " FOREIGN_KEY";
 				}
 				text += "\n";
 			}	
@@ -108,6 +110,38 @@ public class GenerateERD implements IGenerateERD {
 			e.printStackTrace();
 		}
 		return text;
+	}
+	
+	// method to check if column is primary key
+	private static boolean isPrimaryKey(String c[], String tableName) {
+		if(Arrays.asList(c).contains("primaryKey")) {
+			// add table name and primary key into primaryKey map
+			primaryKeys.put(c[0], tableName);
+			return true;
+		}
+		return false;
+	}
+	
+	// method to check if column is foreign key
+	private static boolean isForeignKey(String c[], String tableName) {
+		if(Arrays.asList(c).contains("foreignKey")) {
+			// if the current column is not available into foreignKey map
+			// then add key and create empty list as a value 
+			if(!foreignKeys.containsKey(c[0])) {
+				foreignKeys.put(c[0], new ArrayList<Map<String,String>>());
+			}
+			// add table name with constrain in map object
+			Map<String, String> temp = new HashMap<String, String>();
+			temp.put(tableName, "");
+			// check if unique constrain is available and if available, add it into map
+			if(c.length == 5) {
+				temp.put(tableName, c[4]);
+			}
+			// add table name and constrain into foreignKey map 
+			foreignKeys.get(c[0]).add(temp);
+			return true;
+		}
+		return false;
 	}
 	
 	// method to get all the tables of database
