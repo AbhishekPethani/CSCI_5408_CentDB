dd61f904a9d9dc4c4e377bad2687514ee9f8266a
patel11 <vv411034@dal.ca>
2021-12-08 00:02:55 -0400

    added proper validations in analytics

--- src/main/java/analytics/DatabaseAnalyticsImpl.java
+++ src/main/java/analytics/DatabaseAnalyticsImpl.java
@@ -1,7 +1,11 @@
 package analytics;
 
+import DataModelling.GenerateERD;
+import log_management.EventLogsImpl;
 import log_management.LogDirectorySetup;
+import log_management.constant.Event;
 import log_management.constant.Files;
+import log_management.model.EventLogsModel;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,36 +26,47 @@ public class DatabaseAnalyticsImpl implements Analytics {
     public void getAnalytics(String query) {
         List<String> queryTokens = new ArrayList<>(Arrays.asList(query.split(" ")));
         String databaseName = queryTokens.get(2);
-        LogDirectorySetup logDirSetup = LogDirectorySetup.getInstance();
-        File logFilesDirectory = logDirSetup.setupDirectories();
-        try {
-            File queryLogFile = new File(logFilesDirectory.getCanonicalPath() + "\\" + Files.QUERY_LOGS_FILE);
-            Scanner scan = new Scanner(new FileInputStream(queryLogFile));
-            List<String> userList = new ArrayList<>();
-            while (scan.hasNextLine()) {
-                String logEntry = scan.nextLine();
-                List<String> logTokens = new ArrayList<>(Arrays.asList(logEntry.split(" \\|:\\| ")));
-                String username = logTokens.get(1).replaceAll("Username: ", "");
-                userList.add(username);
-            }
-            Set<String> uniqueUsers = new HashSet<>(userList);
-            Map<String, Integer> userQueryCount = new HashMap<>();
-            for (String user : uniqueUsers) {
-                int queryCount = 0;
-                scan = new Scanner(new FileInputStream(queryLogFile));
+        if (GenerateERD.databaseExist(databaseName)) {
+            LogDirectorySetup logDirSetup = LogDirectorySetup.getInstance();
+            File logFilesDirectory = logDirSetup.setupDirectories();
+            try {
+                File queryLogFile = new File(logFilesDirectory.getCanonicalPath() + "\\" + Files.QUERY_LOGS_FILE);
+                Scanner scan = new Scanner(new FileInputStream(queryLogFile));
+                List<String> userList = new ArrayList<>();
                 while (scan.hasNextLine()) {
                     String logEntry = scan.nextLine();
-                    if (logEntry.contains(user) && logEntry.contains(databaseName)) {
-                        queryCount += 1;
-                        userQueryCount.put(user, queryCount);
+                    List<String> logTokens = new ArrayList<>(Arrays.asList(logEntry.split(" \\|:\\| ")));
+                    String username = logTokens.get(1).replaceAll("Username: ", "");
+                    userList.add(username);
+                }
+                Set<String> uniqueUsers = new HashSet<>(userList);
+                Map<String, Integer> userQueryCount = new HashMap<>();
+                for (String user : uniqueUsers) {
+                    int queryCount = 0;
+                    scan = new Scanner(new FileInputStream(queryLogFile));
+                    while (scan.hasNextLine()) {
+                        String logEntry = scan.nextLine();
+                        if (logEntry.contains(user) && logEntry.contains(databaseName)) {
+                            queryCount += 1;
+                            userQueryCount.put(user, queryCount);
+                        }
                     }
                 }
+                for (String user : uniqueUsers) {
+                    System.out.println("User " + user + " Submitted " + userQueryCount.get(user) + " Queries On " + databaseName);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            for (String user : uniqueUsers) {
-                System.out.println("User " + user + " Submitted " + userQueryCount.get(user) + " Queries On " + databaseName);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            String errorMessage = "Database Does Not Exist";
+            EventLogsModel eventLogsModel = EventLogsModel.getInstance();
+            eventLogsModel.setEventType(Event.CRASH_REPORT);
+            eventLogsModel.setMessage(errorMessage);
+            eventLogsModel.setDatabaseName(databaseName);
+            EventLogsImpl eventLogs = EventLogsImpl.getInstance();
+            eventLogs.eventLogsEntry(eventLogsModel);
         }
+
     }
 }
